392496c2dfff6f56e37c35cf7432b01d18fcac32
Murilo Viscondi <murilo.viscondi@datlo.com>
2025-07-23 23:58:00 -0300

    refactor: remove unnecessary items

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import { StyledThemeProvider,system } from '@lib'
+import { StyledThemeProvider, system } from '@lib'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.tsx'
-
-import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StyledThemeProvider>
